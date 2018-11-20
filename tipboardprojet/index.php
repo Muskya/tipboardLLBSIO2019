0ebcc9b3d4b61b72e16e6c3a6835ae6c93ec1856
Theo <btssio2@10.221.203.29>
2018-11-20 10:23:35 +0100

    Liaison git / projet effectuée

---
+++ tipboardprojet/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- Premier commentaire test GIT -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Projet bonjour je suis le projet</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
