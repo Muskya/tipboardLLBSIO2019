5cf8c71181ef915e90fa04d2be687194d6d17bf7
[Saïd] <btssio2@10.221.203.12>
2018-11-27 09:02:02 +0100

    Exécution d'une requête API Tipboard

---
+++ tipboardprojet/Glances.js
@@ -0,0 +1,16 @@
+//const fetch = require('node-fetch');
+//
+//const URL = 'http://10.22.40.192:61208/api/3/';
+//fetch(URL, {  
+//    method: 'GET', 
+//    headers: {
+//        'Content-Type': 'application/x-www-form-urlencoded'
+//    },  
+//    body: 'cpu'
+//})
+//.then(function (data) {  
+//  console.log('Request success: ', data);  
+//})  
+//.catch(function (error) {  
+//  console.log('Request failure: ', error);  
+//});
---
+++ tipboardprojet/Tipboard.js.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+const URL = 'http://10.22.40.192:7272/api/v0.1/78359fa056ba4a9b8d6288d525e27324/push';
+fetch(URL, {  
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },  
+    body: 'tile=text&key=un&data={"text":"Bonjour"}'
+})
+.then(function (data) {  
+  console.log('Request success: ', data);  
+})  
+.catch(function (error) {  
+  console.log('Request failure: ', error);  
+});
