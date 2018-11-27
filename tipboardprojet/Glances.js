c5f2be08e9e976a67fa5d674aa1a619c643b8264
ThéoT <>
2018-11-27 10:18:39 +0100

    Requête GET sur Glances fonctionnelle (CPU).

--- tipboardprojet/Glances.js
+++ tipboardprojet/Glances.js
@@ -1,16 +1,20 @@
-//const fetch = require('node-fetch');
-//
-//const URL = 'http://10.22.40.192:61208/api/3/';
-//fetch(URL, {  
-//    method: 'GET', 
-//    headers: {
-//        'Content-Type': 'application/x-www-form-urlencoded'
-//    },  
-//    body: 'cpu'
-//})
-//.then(function (data) {  
-//  console.log('Request success: ', data);  
-//})  
-//.catch(function (error) {  
-//  console.log('Request failure: ', error);  
-//});
+var fetch = require('node-fetch');
+var URL = 'http://10.22.40.192:61208/api/3/cpu';
+
+fetch(URL, {  
+    method: 'GET', 
+    headers: {
+        'Content-Type': 'application/json',
+    }  
+    //body: 'cpu'
+})
+ 
+.then((resp) => resp.json()) 
+.then(function (resp) {  
+  console.log('Request success: ', resp);  
+})  
+
+.catch(function (error) {  
+  console.log('Request failure: ', error);  
+});
+
