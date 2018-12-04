39f7b5a589395d65dfc138ddf491d2935f423c76
Theo <btssio2@10.221.203.29>
2018-12-04 11:59:22 +0100

    Fonctions GET/POST CPU / Mémoire fonctionnelles. Infos machines récoltées aussi.

--- tipboardprojet/Glances.js
+++ tipboardprojet/Glances.js
@@ -3,9 +3,41 @@ var cpu;
 var URLGetGlances = 'http://10.22.40.192:61208/api/3/';
 var URLPostTipboard = 'http://10.22.40.192:7272/api/v0.1/78359fa056ba4a9b8d6288d525e27324/push'
 
-GetPostCPU("baguette");
+GetPostCPU();
+GetPostMem();
+GetPostMachine();
 
-function GetPostCPU(itemCpu) {
+function GetPostMachine() {
+    fetch('http://10.22.40.192:61208/api/3/system', {  
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+        }  
+    })
+    .then((resp) => resp.json()) 
+    .then(function (resp) { 
+
+        fetch(URLPostTipboard, {  
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },  
+            //body: 'tile=fancy_listing&key=a1&data={"items": ["OS: ' + resp.os_name+ '", "Distrib: ' + resp.linux_distro + '", "Nom: ' + resp.hostname + '"]}'
+            body: 'tile=fancy_listing&key=a1&data=[{"label":"OS", "text": "' + resp.os_name + '", "description": "' + resp.linux_distro + '"}]'
+        })
+        .then(function (data) {  
+           console.log('Request success: ', data);  
+        })  
+        .catch(function (error) {  
+            console.log('Request failure: ', error);  
+        }); 
+    })  
+    .catch(function (error) {  
+        console.log('Request failure: ', error);  
+    });
+}
+
+function GetPostCPU() {
     // GET //
     fetch('http://10.22.40.192:61208/api/3/cpu', {  
         method: 'GET', 
@@ -21,7 +53,7 @@ function GetPostCPU(itemCpu) {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },  
-            body: 'tile=pie_chart&key=a1&data={"title": "CPU Values", "pie_data": [["idle", ' + resp.idle + '],\n\
+            body: 'tile=pie_chart&key=a2&data={"title": "Valeurs CPU", "pie_data": [["idle", ' + resp.idle + '],\n\
                      ["system", ' +resp.system+ '], ["user", '+resp.user+']]}'
         })
         .then(function (data) {  
@@ -37,7 +69,38 @@ function GetPostCPU(itemCpu) {
         console.log('Request failure: ', error);  
     });}
 
-    // POST //
+function GetPostMem() {
+    // GET //
+    fetch('http://10.22.40.192:61208/api/3/mem', {  
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+        }  
+    })
+    .then((resp) => resp.json()) 
+    .then(function (resp) { 
+
+        fetch(URLPostTipboard, {  
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },  
+            body: 'tile=listing&key=a3&data={"title": "Mémoire", "items": ["Totale: ' 
+                    + Math.round(resp.total/1000000) + ' Mo", "Libre: ' + Math.round(resp.free/1000000) + ' Mo", "Utilisée: ' 
+                    + Math.round(resp.used/1000000) + ' Mo"]}'
+        })
+        .then(function (data) {  
+           console.log('Request success: ', data);  
+        })  
+        .catch(function (error) {  
+            console.log('Request failure: ', error);  
+        }); 
+
+        //console.log('CPU Idle: ', resp.idle);  
+    })  
+    .catch(function (error) {  
+        console.log('Request failure: ', error);  
+    });}
     
 
 
